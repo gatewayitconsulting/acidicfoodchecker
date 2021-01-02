8510776a12bebf253cf6bb24d97a98c50c3b6b17
John Montanye <43389272+gatewayitconsulting@users.noreply.github.com>
2021-01-02 16:03:06 -0600

    Implemented basic website functionality
    Home Page
    Page Not Found Page and handling
    Message and Window Services
    Basic routing samples
    Images folder
    Basic styles default from GITC

--- phoodcalc-app/src/app/app-routing.module.ts
+++ phoodcalc-app/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HomeComponent } from './pages/home/home.component';
+// import { ContactComponent } from './pages/contact/contact.component';
+// import { AboutComponent } from './pages/about/about.component';
+// import { NewsStoryComponent } from './components/news-story/news-story.component';
+// import { NewsComponent } from './pages/news/news.component';
 
-const routes: Routes = [];
+
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled', // To add fragment URL /contact-us#getInTouch = routerLink="/contact-us" fragment="getInTouch"
+  scrollOffset: [0, 64],
+  enableTracing: true, // <-- debugging purposes only
+};
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  // { path: 'contact-us', component: ContactComponent },
+  // { path: 'about', component: AboutComponent },
+  // { path: 'news/:id', component: NewsStoryComponent },
+  // { path: 'news', component: NewsComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, routerOptions)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- phoodcalc-app/src/app/app.module.ts
+++ phoodcalc-app/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WINDOW_PROVIDERS } from './services/window/window.service';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    WINDOW_PROVIDERS
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
